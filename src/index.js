1d5c4e5341a496689895f7d2bcd800de7bb6125b
srahmed <srehanpk@gmail.com>
2020-06-11 22:12:41 +0500

    using state

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Room from './Room.js';
+import Temp from './Temp.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <>
+    <Room image1="bulb-on.gif" image2="bulb-off.gif" />
+    <Temp />
+ 
+ </>   ,
+
   document.getElementById('root')
 );
 
